05b44777c7c770d244d56dfea000f87261810c16
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-25 14:32:15 +0500

    dependencyinjection

--- IT-TradeSoft/src/TradeSoft.Application/DependencyInjection.cs
+++ IT-TradeSoft/src/TradeSoft.Application/DependencyInjection.cs
@@ -2,16 +2,14 @@
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
-namespace TradeSoft.Application
+namespace TradeSoft.Application;
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+    public static IServiceCollection AddAplication(this IServiceCollection services)
     {
-        public static IServiceCollection AddAplication(this IServiceCollection services)
-        {
-            services.AddMediatR(Assembly.GetExecutingAssembly());
+        services.AddMediatR(Assembly.GetExecutingAssembly());
 
-            return services;
-        }
-
+        return services;
     }
+
 }
