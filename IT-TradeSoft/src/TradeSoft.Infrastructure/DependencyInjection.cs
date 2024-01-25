20394df3dc8b6c2648aa395140b2393ba50ea5e2
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-25 14:32:33 +0500

    depe

--- IT-TradeSoft/src/TradeSoft.Infrastructure/DependencyInjection.cs
+++ IT-TradeSoft/src/TradeSoft.Infrastructure/DependencyInjection.cs
@@ -4,18 +4,16 @@ using Microsoft.Extensions.DependencyInjection;
 using TradeSoft.Application.Absreactions;
 using TradeSoft.Infrastructure.Data;
 
-namespace TradeSoft.Infrastructure
+namespace TradeSoft.Infrastructure;
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        services.AddDbContext<ITradeSoftDBContext, TradeSoftDBContext>(options =>
         {
-            services.AddDbContext<ITradeSoftDBContext, TradeSoftDBContext>(options =>
-            {
-                options.UseNpgsql(configuration.GetConnectionString("Defoult"));
-            });
+            options.UseNpgsql(configuration.GetConnectionString("Defoult"));
+        });
 
-            return services;
-        }
+        return services;
     }
 }
