934fc5bd019783c323f421ba6aff5d2ba8753f2a
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-26 23:35:41 +0500

    asa

---
+++ IT-TradeSoft/src/TradeSoft.Application/Common/Helpers/MediaHelper.cs
@@ -0,0 +1,64 @@
+﻿using TradeSoft.Domain.Exceptions;
+using TradeSoft.Domain.Exceptions.ImageExceptions;
+
+namespace TradeSoft.Application.Common.Helpers
+{
+    public class MediaHelper
+    {
+        public static string MakeImageName(string filename)
+        {
+            FileInfo fileInfo = new FileInfo(filename);
+
+            string[] ImageExtension = GetImageExtensions();
+
+            if (ImageExtension.Any(x => x == fileInfo.Extension))
+            {
+                string extension = fileInfo.Extension;
+                string name = "IMG_" + Guid.NewGuid() + extension;
+                return name;
+            }
+            throw new ImageNotValidExceptions();
+        }
+
+        public static string MakeFileName(string filename)
+        {
+            FileInfo fileInfo = new FileInfo(filename);
+
+            string[] ImageExtension = GetFileExtensions();
+
+            if (ImageExtension.Any(x => x == fileInfo.Extension))
+            {
+                string extension = fileInfo.Extension;
+                string name = "FILE_" + Guid.NewGuid() + extension;
+                return name;
+            }
+            throw new ImageNotValidExceptions();
+        }
+
+        public static string[] GetImageExtensions()
+        {
+            return new string[]
+            {
+            ".jpg", ".jpeg",
+            ".png",
+            ".bmp",
+            ".svg"
+            };
+        }
+
+        public static string[] GetFileExtensions()
+        {
+            return new string[]
+            {
+            ".doc",
+            ".docx",
+            ".xls",
+            ".xlsx",
+            ".pdf",
+            ".txt",
+            ".zip",
+            ".rar",
+            };
+        }
+    }
+}
