279658d2134199ef44b695e6af42962e9598117e
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-26 23:38:12 +0500

    asas

---
+++ IT-TradeSoft/src/TradeSoft.Domain/Exceptions/AddressExceptions/AddressNotFoundExceptions.cs
@@ -0,0 +1,10 @@
+﻿namespace TradeSoft.Domain.Exceptions.AddressExceptions
+{
+    public class AddressNotFoundExceptions : NotFoundException
+    {
+        public AddressNotFoundExceptions()
+        {
+            TitleMessage = "Address not Found!";
+        }
+    }
+}
