099d61b34051fedf4cc9ab3b5094f229338fda11
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-26 23:36:55 +0500

    sasas

---
+++ IT-TradeSoft/src/TradeSoft.Application/UseCases/ProjectCalculations/Handler/CreateProjectCalculationCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using TradeSoft.Application.Absreactions;
+using TradeSoft.Application.FileServices;
+using TradeSoft.Application.UseCases.ProjectCalculations.Commands;
+using TradeSoft.Domain.Entities.ProjectCalculations;
+
+namespace TradeSoft.Application.UseCases.ProjectCalculations.Handler
+{
+    public class CreateProjectCalculationCommandHandler : IRequestHandler<CreateProjectCalculationCommand, bool>
+    {
+        private readonly ITradeSoftDBContext _dbContext;
+        private readonly IFileService _fileService;
+        public CreateProjectCalculationCommandHandler(ITradeSoftDBContext dbContext, IFileService fileService)
+        {
+            _dbContext = dbContext;
+            _fileService = fileService;
+        }
+
+        public async Task<bool> Handle(CreateProjectCalculationCommand request, CancellationToken cancellationToken)
+        {
+            string filepage = await _fileService.UploadFileAsync(request.FilePath);
+
+            var result = new ProjectCalculation()
+            {
+                Name = request.Name,
+                Email = request.Email,
+                PhoneNumber = request.PhoneNumber,
+                FilePath = filepage,
+                CreatedAt = DateTime.UtcNow,
+            };
+
+            await _dbContext.Calculations.AddAsync(result);
+            var res = await _dbContext.SaveChangesAsync(cancellationToken);
+            return res > 0;
+        }
+    }
+}
