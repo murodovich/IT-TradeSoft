2217f196d5be7db0cf13dc312b44cf349aaedc94
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-26 23:37:43 +0500

    sasa

---
+++ IT-TradeSoft/src/TradeSoft.Application/UseCases/Questions/Query/GetByIdQuestionQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using TradeSoft.Domain.Entities.Questions;
+
+namespace TradeSoft.Application.UseCases.Questions.Query
+{
+    public class GetByIdQuestionQuery  : IRequest<Question>
+    {
+        public int Id { get; set; }
+    }
+}
