ddb257381eb31d18cc57d5d29f37a47fda42e9ae
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-26 23:37:34 +0500

    aasas

---
+++ IT-TradeSoft/src/TradeSoft.Application/UseCases/Questions/Handler/UpdateQuestionsCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TradeSoft.Application.Absreactions;
+using TradeSoft.Application.UseCases.Questions.Commond;
+using TradeSoft.Domain.Exceptions.QuestionExceptions;
+
+namespace TradeSoft.Application.UseCases.Questions.Handler
+{
+    public class UpdateQuestionsCommandHandler : IRequestHandler<UpdateQuestionsCommand, bool>
+    {
+        private readonly ITradeSoftDBContext _dbContext;
+        
+
+        public UpdateQuestionsCommandHandler(ITradeSoftDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> Handle(UpdateQuestionsCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _dbContext.Question.FirstOrDefaultAsync(x => x.Id == request.Id);
+            if (result == null) throw new QuestionNotFoundException();
+
+            result.Name = request.Name;
+            result.PhoneNumber = request.PhoneNumber;
+            result.Questionies = request.Questionies;
+            result.UpdatedAt = DateTime.UtcNow;
+
+            _dbContext.Question.Update(result);
+            var res = await _dbContext.SaveChangesAsync(cancellationToken);
+            return res > 0;
+        }
+    }
+}
