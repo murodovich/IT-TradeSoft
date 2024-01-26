0191180610bec79a363328857c0f61cb143699c4
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-26 23:35:36 +0500

    Dbcontext

---
+++ IT-TradeSoft/src/TradeSoft.Application/Absreactions/ITradeSoftDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TradeSoft.Domain.Entities.Addreses;
+using TradeSoft.Domain.Entities.ProjectCalculations;
+using TradeSoft.Domain.Entities.Projects;
+using TradeSoft.Domain.Entities.Questions;
+
+namespace TradeSoft.Application.Absreactions
+{
+    public interface ITradeSoftDBContext
+    {
+
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<ProjectCalculation> Calculations { get; set; }
+        public DbSet<Question> Question { get; set; }
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
