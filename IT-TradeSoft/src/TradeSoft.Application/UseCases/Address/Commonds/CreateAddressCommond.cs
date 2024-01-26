b8c0b0c239da49392eb374a1b40c7e9a62c3f245
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-26 23:35:54 +0500

    asa

---
+++ IT-TradeSoft/src/TradeSoft.Application/UseCases/Address/Commonds/CreateAddressCommond.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace TradeSoft.Application.UseCases.Address.Commonds
+{
+    public class CreateAddressCommond : IRequest<bool>
+    {
+        public string PhoneNumber { get; set; }
+        public string City { get; set; }
+        public string Email { get; set; }
+    }
+}
