e5cabc429cd63b4a7db3dc9b4cca06a3e3eff55e
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-26 23:34:08 +0500

    fdfd

---
+++ IT-TradeSoft/src/TradeSoft.Api/Controllers/ProjectCalculationController.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TradeSoft.Application.UseCases.ProjectCalculations.Commands;
+using TradeSoft.Application.UseCases.ProjectCalculations.Query;
+
+namespace TradeSoft.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProjectCalculationController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProjectCalculationController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateProjectCalculatorAsync([FromForm] CreateProjectCalculationCommand command)
+        {
+            var res =  await _mediator.Send(command);
+            return Ok(res);
+        }
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllProjectCalculationAsync()
+        {
+            var result = await _mediator.Send(new GetAllProjectCalculationQuery());
+            return Ok(result);
+        }
+
+
+    }
+}
