0f7189528b359ea2362558ac9445f62c66f4b636
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-26 23:36:05 +0500

    asas

---
+++ IT-TradeSoft/src/TradeSoft.Application/UseCases/Address/Handlers/GetByIdAddressQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TradeSoft.Application.Absreactions;
+using TradeSoft.Application.UseCases.Address.Query;
+using TradeSoft.Domain.Exceptions.AddressExceptions;
+
+namespace TradeSoft.Application.UseCases.Address.Handlers
+{
+    public class GetByIdAddressQueryHandler : IRequestHandler<GetByIdAddressQuery, Domain.Entities.Addreses.Address>
+    {
+        private readonly ITradeSoftDBContext _dbContext;
+
+        public GetByIdAddressQueryHandler(ITradeSoftDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Domain.Entities.Addreses.Address> Handle(GetByIdAddressQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _dbContext.Addresses.FirstOrDefaultAsync(x => x.Id == request.Id);
+            if (result == null) throw new AddressNotFoundExceptions();
+            return result;
+        }
+    }
+}
