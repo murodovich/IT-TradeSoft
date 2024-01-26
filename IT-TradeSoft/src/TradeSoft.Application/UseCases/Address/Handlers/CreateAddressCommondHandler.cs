c2ae308bd39db9883529815bc347f6945107ee04
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-26 23:35:58 +0500

    asas

---
+++ IT-TradeSoft/src/TradeSoft.Application/UseCases/Address/Handlers/CreateAddressCommondHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using TradeSoft.Application.Absreactions;
+using TradeSoft.Application.UseCases.Address.Commonds;
+
+
+namespace TradeSoft.Application.UseCases.Address.Handlers
+{
+    public class CreateAddressCommondHandler : IRequestHandler<CreateAddressCommond, bool>
+    {
+        private readonly ITradeSoftDBContext _dbContext;
+
+        public CreateAddressCommondHandler(ITradeSoftDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> Handle(CreateAddressCommond request, CancellationToken cancellationToken)
+        {
+            var result = new Domain.Entities.Addreses.Address()
+            {
+                PhoneNumber = request.PhoneNumber,
+                Email = request.Email,
+                City = request.City,
+                CreatedAt = DateTime.UtcNow,
+            };
+
+            await _dbContext.Addresses.AddAsync(result);
+            var res = await _dbContext.SaveChangesAsync(cancellationToken);
+            return res > 0;
+        }
+    }
+}
