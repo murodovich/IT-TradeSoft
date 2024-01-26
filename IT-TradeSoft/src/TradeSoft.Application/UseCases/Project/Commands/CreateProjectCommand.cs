6ed879706d986d60383b1bbb073ea0245bd2c43b
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-26 23:36:17 +0500

    sasa

---
+++ IT-TradeSoft/src/TradeSoft.Application/UseCases/Project/Commands/CreateProjectCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace TradeSoft.Application.UseCases.Project.Commands
+{
+    public class CreateProjectCommand : IRequest<bool>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public IFormFile ImagePath { get; set; }
+    }
+}
