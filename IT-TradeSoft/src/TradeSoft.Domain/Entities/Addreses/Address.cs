410650f9fec27ab4d4c662231dbd789579566e86
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-26 23:38:01 +0500

    asas

---
+++ IT-TradeSoft/src/TradeSoft.Domain/Entities/Addreses/Address.cs
@@ -0,0 +1,12 @@
+﻿namespace TradeSoft.Domain.Entities.Addreses
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string PhoneNumber { get; set; }
+        public string City { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
