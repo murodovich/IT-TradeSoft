c5b309cfd2fbbebcc6f04d5497767af67ff6ea74
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-26 23:37:19 +0500

    sassa

---
+++ IT-TradeSoft/src/TradeSoft.Application/UseCases/Questions/Handler/CreateQuestionCommondHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using TradeSoft.Application.Absreactions;
+using TradeSoft.Application.UseCases.Questions.Commond;
+
+namespace TradeSoft.Application.UseCases.Questions.Handler
+{
+    public class CreateQuestionCommondHandler : IRequestHandler<CreateQuestionCommand, bool>
+    {
+        private readonly ITradeSoftDBContext _dbContext;
+
+        public CreateQuestionCommondHandler(ITradeSoftDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> Handle(CreateQuestionCommand request, CancellationToken cancellationToken)
+        {
+            var result = new Domain.Entities.Questions.Question()
+            {
+                Name = request.Name,
+                PhoneNumber = request.PhoneNumber,
+                Questionies = request.Questionies,
+                CreatedAt = DateTime.UtcNow,
+
+            };
+
+            await _dbContext.Question.AddAsync(result);
+            var res = await _dbContext.SaveChangesAsync(cancellationToken);
+            return res > 0;
+        }
+    }
+}
