fdd0617648c36be7150a07e03fe71a8762ab038a
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-26 23:37:07 +0500

    sasa

---
+++ IT-TradeSoft/src/TradeSoft.Application/UseCases/Questions/Commond/CreateQuestionCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace TradeSoft.Application.UseCases.Questions.Commond
+{
+    public class CreateQuestionCommand : IRequest<bool>
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Questionies { get; set; }
+
+    }
+}
