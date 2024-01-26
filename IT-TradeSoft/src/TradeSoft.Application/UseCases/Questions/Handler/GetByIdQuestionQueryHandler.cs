4d9cabbb394c2ba036372c803f24a39f7ebd2b2d
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-26 23:37:30 +0500

    sasas

---
+++ IT-TradeSoft/src/TradeSoft.Application/UseCases/Questions/Handler/GetByIdQuestionQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TradeSoft.Application.Absreactions;
+using TradeSoft.Application.UseCases.Questions.Query;
+using TradeSoft.Domain.Entities.Questions;
+
+namespace TradeSoft.Application.UseCases.Questions.Handler
+{
+    public class GetByIdQuestionQueryHandler : IRequestHandler<GetByIdQuestionQuery, Question>
+    {
+        private readonly ITradeSoftDBContext _dbContext;
+
+        public GetByIdQuestionQueryHandler(ITradeSoftDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Question> Handle(GetByIdQuestionQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _dbContext.Question.FirstOrDefaultAsync(x => x.Id == request.Id);
+            if (result == null) throw new Exception();
+            return result;
+        }
+    }
+}
