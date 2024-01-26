6b0b5dc893248bc1a893f15bccc21f4727d0c22c
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-26 23:37:11 +0500

    asa

---
+++ IT-TradeSoft/src/TradeSoft.Application/UseCases/Questions/Commond/DeleteQuestionsCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace TradeSoft.Application.UseCases.Questions.Commond
+{
+    public class DeleteQuestionsCommand : IRequest<bool>
+    {
+        public int Id { get; set; }
+    }
+}
