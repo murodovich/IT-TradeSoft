9ace6ef62328de1b56de7690285649b07512c6af
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-26 23:34:18 +0500

    dfdfd

---
+++ IT-TradeSoft/src/TradeSoft.Api/Controllers/QuestionController.cs
@@ -0,0 +1,49 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TradeSoft.Application.UseCases.Questions.Commond;
+using TradeSoft.Application.UseCases.Questions.Query;
+
+namespace TradeSoft.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class QuestionController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public QuestionController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllQuestionsAsync()
+        {
+            var result = await _mediator.Send(new GetAllQuestionQuery());
+            return Ok(result);
+        }
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateQuestionsAsync(CreateQuestionCommand command)
+        {
+            var res = await _mediator.Send(command);
+            return Ok(res);
+        }
+        [HttpGet]
+        public async ValueTask<IActionResult> GetByIdQuestionsAsync(int Id)
+        {
+            var result = await _mediator.Send(new GetByIdQuestionQuery() { Id = Id });
+            return Ok(result);
+        }
+        [HttpPut]
+        public async ValueTask<IActionResult> UpdateQuestionsAsync(UpdateQuestionsCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Updated");
+        }
+        [HttpDelete]
+        public async ValueTask<IActionResult> DeleteQuestionAsync(int Id)
+        {
+            await _mediator.Send(new DeleteQuestionsCommand() { Id = Id });
+            return Ok("Deleted");
+        }
+    }
+}
