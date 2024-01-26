e2d1473c28011c03161aa632238496516a30eba2
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-26 23:37:39 +0500

    sasas

---
+++ IT-TradeSoft/src/TradeSoft.Application/UseCases/Questions/Query/GetAllQuestionQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using TradeSoft.Domain.Entities.Questions;
+
+namespace TradeSoft.Application.UseCases.Questions.Query
+{
+    public class GetAllQuestionQuery : IRequest<List<Question>>
+    {
+
+    }
+}
