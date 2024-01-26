90f94cfff178e5f592993244d93c81af6ed65ba6
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-26 23:37:23 +0500

    sasas

---
+++ IT-TradeSoft/src/TradeSoft.Application/UseCases/Questions/Handler/DeleteQuestionsCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TradeSoft.Application.Absreactions;
+using TradeSoft.Application.UseCases.Questions.Commond;
+
+namespace TradeSoft.Application.UseCases.Questions.Handler
+{
+    public class DeleteQuestionsCommandHandler : IRequestHandler<DeleteQuestionsCommand, bool>
+    {
+        private readonly ITradeSoftDBContext _dbContext;
+
+        public DeleteQuestionsCommandHandler(ITradeSoftDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> Handle(DeleteQuestionsCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _dbContext.Question.FirstOrDefaultAsync(x => x.Id == request.Id);
+            if (result == null) throw new Exception();
+
+            _dbContext.Question.Remove(result);
+            var res = await _dbContext.SaveChangesAsync(cancellationToken);
+            return res > 0;
+        }
+    }
+}
