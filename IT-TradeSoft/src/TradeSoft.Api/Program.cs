bd891a3ac235bea3a7ee8f98c848274d783e16e0
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-26 23:35:12 +0500

    dfdf

--- IT-TradeSoft/src/TradeSoft.Api/Program.cs
+++ IT-TradeSoft/src/TradeSoft.Api/Program.cs
@@ -1,22 +1,35 @@
+using TradeSoft.Application;
+using TradeSoft.Application.FileServices;
+using TradeSoft.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder => builder.AllowAnyOrigin()
+                          .AllowAnyHeader()
+                          .AllowAnyMethod());
+});
+builder.Services.AddScoped<IFileService,FileService>();
+builder.Services.AddAplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseCors("AllowSpecificOrigin");
 
 app.UseAuthorization();
 
