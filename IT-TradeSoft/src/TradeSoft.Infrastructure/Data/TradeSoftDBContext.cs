9176b655c24c444c93abdbeae3de10b720712651
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-26 23:38:30 +0500

    sas

---
+++ IT-TradeSoft/src/TradeSoft.Infrastructure/Data/TradeSoftDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TradeSoft.Application.Absreactions;
+using TradeSoft.Domain.Entities.Addreses;
+using TradeSoft.Domain.Entities.ProjectCalculations;
+using TradeSoft.Domain.Entities.Projects;
+using TradeSoft.Domain.Entities.Questions;
+
+namespace TradeSoft.Infrastructure.Data
+{
+    public class TradeSoftDBContext : DbContext,ITradeSoftDBContext
+    {
+        public TradeSoftDBContext(DbContextOptions<TradeSoftDBContext> options) : base(options)
+        {
+            Database.Migrate();
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<ProjectCalculation> Calculations { get; set; }
+        public DbSet<Question> Question { get; set; }
+
+    }
+}
