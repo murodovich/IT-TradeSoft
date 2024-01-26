437516c20f034c8e4db3cdc987cb286061abdd57
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-26 23:36:13 +0500

    asas

---
+++ IT-TradeSoft/src/TradeSoft.Application/UseCases/Address/Query/GetByIdAddressQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace TradeSoft.Application.UseCases.Address.Query
+{
+    public class GetByIdAddressQuery : IRequest<Domain.Entities.Addreses.Address>
+    {
+        public int Id { get; set; }
+    }
+}
