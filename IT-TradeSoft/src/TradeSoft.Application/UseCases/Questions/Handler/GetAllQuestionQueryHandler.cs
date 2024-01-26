b499826caa40ab229518f11df54aee0ab20ca859
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-26 23:37:27 +0500

    sasa

---
+++ IT-TradeSoft/src/TradeSoft.Application/UseCases/Questions/Handler/GetAllQuestionQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TradeSoft.Application.Absreactions;
+using TradeSoft.Application.UseCases.Questions.Query;
+using TradeSoft.Domain.Entities.Questions;
+using TradeSoft.Domain.Exceptions.QuestionExceptions;
+
+namespace TradeSoft.Application.UseCases.Questions.Handler
+{
+    public class GetAllQuestionQueryHandler : IRequestHandler<GetAllQuestionQuery, List<Question>>
+    {
+        private readonly ITradeSoftDBContext _dbContext;
+
+        public GetAllQuestionQueryHandler(ITradeSoftDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Question>> Handle(GetAllQuestionQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _dbContext.Question.ToListAsync();
+            if (result.Count == 0) throw new QuestionNotFoundException();
+            return result;
+        }
+    }
+}
