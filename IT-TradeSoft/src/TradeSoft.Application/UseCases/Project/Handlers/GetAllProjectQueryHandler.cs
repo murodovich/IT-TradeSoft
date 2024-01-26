5718c1c2dcb90c73441e6206c9318b361843cc07
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-26 23:36:33 +0500

    asas

---
+++ IT-TradeSoft/src/TradeSoft.Application/UseCases/Project/Handlers/GetAllProjectQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TradeSoft.Application.Absreactions;
+using TradeSoft.Application.UseCases.Project.Query;
+using TradeSoft.Domain.Exceptions.ProjectCalculationExceptions;
+
+namespace TradeSoft.Application.UseCases.Project.Handlers
+{
+    public class GetAllProjectQueryHandler : IRequestHandler<GetAllProjectQuery, List<Domain.Entities.Projects.Project>>
+    {
+        private readonly ITradeSoftDBContext _dbContext;
+
+        public GetAllProjectQueryHandler(ITradeSoftDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Domain.Entities.Projects.Project>> Handle(GetAllProjectQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _dbContext.Projects.ToListAsync(cancellationToken);
+            if (result.Count == 0) throw new ProjectNotFoundException();
+            return result;
+        }
+    }
+}
