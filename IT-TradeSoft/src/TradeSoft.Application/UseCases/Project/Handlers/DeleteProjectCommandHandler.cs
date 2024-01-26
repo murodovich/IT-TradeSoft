2e7ebab2e61184c6954e619c26ef5e82e66f5832
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-26 23:36:29 +0500

    sasa

---
+++ IT-TradeSoft/src/TradeSoft.Application/UseCases/Project/Handlers/DeleteProjectCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TradeSoft.Application.Absreactions;
+using TradeSoft.Application.UseCases.Project.Commands;
+using TradeSoft.Domain.Exceptions.ImageExceptions;
+
+namespace TradeSoft.Application.UseCases.Project.Handlers
+{
+    public class DeleteProjectCommandHandler : IRequestHandler<DeleteProjectCommand, bool>
+    {
+        private readonly ITradeSoftDBContext _dbContext;
+
+        public DeleteProjectCommandHandler(ITradeSoftDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> Handle(DeleteProjectCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _dbContext.Projects.FirstOrDefaultAsync(x => x.Id == request.Id);
+            if (result == null) throw new ImageNotValidExceptions();
+            _dbContext.Projects.Remove(result);
+            var res = await _dbContext.SaveChangesAsync(cancellationToken);
+            return res > 0;
+        }
+    }
+}
