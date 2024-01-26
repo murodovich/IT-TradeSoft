6c9c1d7696a3d0eb30508a950a64f7dc495a1c5e
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-26 23:36:46 +0500

    sasa

---
+++ IT-TradeSoft/src/TradeSoft.Application/UseCases/Project/Query/GetByIdProjectQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace TradeSoft.Application.UseCases.Project.Query
+{
+    public class GetByIdProjectQuery : IRequest<Domain.Entities.Projects.Project>
+    {
+        public int Id { get; set; }
+    }
+}
