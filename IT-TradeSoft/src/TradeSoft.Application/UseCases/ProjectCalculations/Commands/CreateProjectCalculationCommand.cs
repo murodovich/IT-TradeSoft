a37ad9cc1f3bd484f589aa7379994e464c96b8d0
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-26 23:36:51 +0500

    sasa

---
+++ IT-TradeSoft/src/TradeSoft.Application/UseCases/ProjectCalculations/Commands/CreateProjectCalculationCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace TradeSoft.Application.UseCases.ProjectCalculations.Commands
+{
+    public class CreateProjectCalculationCommand : IRequest<bool>
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public IFormFile FilePath { get; set; }
+    }
+}
