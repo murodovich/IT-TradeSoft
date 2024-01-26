8d36dea916d5fb5d6e4ce8ab936518f1b52effad
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-26 23:38:25 +0500

    sas a

---
+++ IT-TradeSoft/src/TradeSoft.Domain/Entities/ProjectCalculations/ProjectCalculation.cs
@@ -0,0 +1,13 @@
+﻿namespace TradeSoft.Domain.Entities.ProjectCalculations
+{
+    public class ProjectCalculation
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string FilePath { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ IT-TradeSoft/src/TradeSoft.Domain/Entities/Questions/Question.cs
@@ -0,0 +1,12 @@
+﻿namespace TradeSoft.Domain.Entities.Questions
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Questionies { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
