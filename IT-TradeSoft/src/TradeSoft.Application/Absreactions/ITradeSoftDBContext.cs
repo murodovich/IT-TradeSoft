19b3b02eef7f8f0d56a40b49f6994fe32b6417ac
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-01-29 14:37:40 +0500

    asas

--- IT-TradeSoft/src/TradeSoft.Application/Absreactions/ITradeSoftDBContext.cs
+++ IT-TradeSoft/src/TradeSoft.Application/Absreactions/ITradeSoftDBContext.cs
@@ -3,12 +3,10 @@ using TradeSoft.Domain.Entities.Addreses;
 using TradeSoft.Domain.Entities.ProjectCalculations;
 using TradeSoft.Domain.Entities.Projects;
 using TradeSoft.Domain.Entities.Questions;
-
 namespace TradeSoft.Application.Absreactions
 {
     public interface ITradeSoftDBContext
     {
-
         public DbSet<Address> Addresses { get; set; }
         public DbSet<Project> Projects { get; set; }
         public DbSet<ProjectCalculation> Calculations { get; set; }
